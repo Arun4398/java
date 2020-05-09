7d2d5db6ecd1cd8d6a18c85a6fb99bb028895782
Arun4398 <62743150+Arun4398@users.noreply.github.com>
2020-05-09 16:10:08 +0530

    Add files via upload

---
+++ Method overloading/7.Method Overloading.java
@@ -0,0 +1,33 @@
+METHOD OVERLOADING:
+
+	=>Developing multiple methods within the same class with the same name which may differ In
+		->Number of arguments
+		->Data type of arguments
+		->Order of arguments
+		is called method overloading.
+
+	=>Two Overloaded Methods can have different
+		->Access Modifiers.
+		->Access Specifiers.
+		->Return Type.
+
+	  eg: println(); is the best example for method overloading.
+
+Advantages:
+
+	=>It is easy to remember one method which may perfrom similar operation with different arguments.
+
+Disadvantages:
+
+	=>If proper documentation are not maintained for every overloaded method, it will be diffcult for the 
+		new developer to undtestand behaviours of every methods.
+
+
+		eg:
+			1.methodoverloadingMain.java
+
+		
+
+
+
+
