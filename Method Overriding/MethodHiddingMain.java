5965fb1d4d9b8c70bc054552e7c400ce1a62ed73
Arun4398 <62743150+Arun4398@users.noreply.github.com>
2020-05-12 14:09:58 +0530

    Add files via upload

--- Method Overriding/MethodHiddingMain.java
+++ Method Overriding/MethodHiddingMain.java
@@ -4,8 +4,12 @@ class Superclass
  {
 System.out.println("Super class");
  }
+ public void test()
+ {
+ 	System.out.println("test() 0f Superclass");
+ }
 }
-class Subclass
+class Subclass extends Superclass
 {
  public static void Hidding()
  {
@@ -21,5 +25,6 @@ class MethodHiddingMain
 
 		Subclass s2 =new Subclass();
 		s2.Hidding();
+		s2.test();
 	}
 }
