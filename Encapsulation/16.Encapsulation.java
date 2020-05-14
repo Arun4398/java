50e985915fa087c9c02ba005bfa0abdc6d914c98
Arun4398 <62743150+Arun4398@users.noreply.github.com>
2020-05-14 14:49:53 +0530

    Add files via upload

---
+++ Encapsulation/16.Encapsulation.java
@@ -0,0 +1,14 @@
+ENCAPSULATION:
+	
+	=>Provideing Security for the data members by declerating them as private and grant the access through getter and setter 
+		method is called encapulation.
+
+	=>Getter method returns current value of private data members.
+
+	=>Return type of the Getter method deponds upon the data type if private members
+
+	=>Setter method update the value of the private members.
+
+	=>Return type of the setter method will be usally declared as void but it can be changed according application requriment
+
+	=>Encapulation is used to protect the data members of the class against the invalid values.
