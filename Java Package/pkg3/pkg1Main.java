e782b5a533ec8739d4d94cde3b842c1ecb88fee5
Arun4398 <62743150+Arun4398@users.noreply.github.com>
2020-05-29 08:21:14 +0530

    Add files via upload

---
+++ Java Package/pkg3/pkg1Main.java
@@ -0,0 +1,14 @@
+package com.jspiders.pkg3;
+
+public class pkg1Main
+{
+	public static void test() 
+	{
+		System.out.println("test() of pkg3.Sample class");
+	}
+
+	public void display()
+	{
+		System.out.println("display() of pkg3.Sample class");
+	}
+}
